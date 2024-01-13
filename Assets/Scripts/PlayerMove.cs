e752d55d1c20a9fccba840fbd34ab99c93286c5d
Song <Cvbvbb51@konkuk.ac.kr>
2024-01-13 11:08:45 +0900

    B16 Update

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,9 +6,8 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float maxSpeed;
-
+    public float jumpPower;
     Rigidbody2D rigid;
-
     SpriteRenderer spriteRenderer;
     Animator anim;
 
@@ -20,6 +19,14 @@ public class PlayerMove : MonoBehaviour
     }
     private void Update() //Update : 단발적인 키 입력
     {
+
+        //Jump
+        if (Input.GetButtonDown("Jump") && !anim.GetBool("isJumping"))
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            anim.SetBool("isJumping", true);
+        }
+
         //Stop Speed
         if (Input.GetButtonUp("Horizontal")) //뗏을때
         {
@@ -42,8 +49,8 @@ public class PlayerMove : MonoBehaviour
         {
             anim.SetBool("isWalking", true);
 
-        }
 
+        }
     }
 
     private void FixedUpdate() //1초에 걍 50번 준다. 즉 누르면 누를수록 엄청 빨라짐(Addforce)->가속을 무한으로 받는다.
@@ -61,7 +68,32 @@ public class PlayerMove : MonoBehaviour
         else if (rigid.velocity.x < maxSpeed * (-1)) //Left Max Speed
         {
             rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
-
         }
+
+        //Landing Platform
+        if (rigid.velocity.y < 0)
+        {
+            Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+            if (rayHit.collider != null)
+            {
+                if (rayHit.distance < 0.5f)
+                {
+                    anim.SetBool("isJumping", false);
+                    Debug.Log(rayHit.collider.name);
+
+                }
+            }
+        }
+
+
+
     }
+
+
+
 }
+
+
